c98319bd26cfee7c26e028ebcc03959b7b251add
AhmedMohamed50 <85414834+AhmedMohamed50@users.noreply.github.com>
2024-06-10 14:07:15 +0300

    Update

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Profile />
-      <Qualifications />
+      {/* <Qualifications /> */}
       <Meals />
     </div>
   );
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -12,22 +12,31 @@ function Profile() {
     };
 
     return (
-        <div className="column">
-            <img src={ProfilePicture} className="profile-picture" alt="Profile"/>
-            <h2>Ahmed M. AbdelRahim Maree</h2>
-            {displayBio ? (
-                <div>
-                    <h3>I am Computer Engineer</h3>
-                    <h4>This is my first react project</h4>
-                    <p>I want to learn more about React to work as a front-end developer</p>
-                    <button onClick={toggle} className='btn'>Hide</button>
-                </div>
-            ) : (
-                <button onClick={toggle} className='btn'>Show more</button>
-            )}
-            
+        <div className='App'>
+            <div className="column">
+                <img src={ProfilePicture} className="profile-picture" alt="Profile"/>
+                <h2>Ahmed M. AbdelRahim Maree</h2>
+                {displayBio ? (
+                    <div>
+                        <h3>I am Computer Engineer</h3>
+                        <h4>This is my first react project</h4>
+                        <p>I want to learn more about React to work as a front-end developer</p>
+                        <button onClick={toggle} className='btn'>Hide</button>
+                    </div>
+                ) : (
+                    <button onClick={toggle} className='btn'>Show more</button>
+                )}
+            </div>
+
+            <div className="column">
+                <h2>Education: Faculty of Engineering Ain Shams University</h2> <hr />
+                <h3>I am Computer Engineer</h3> <hr />
+                <h4>this is my first react project</h4> <hr />
+                
+                <p>I want to learn more about React to work as a front-end developer</p>
+                
+            </div>
         </div>
-        
     );
 }
 
